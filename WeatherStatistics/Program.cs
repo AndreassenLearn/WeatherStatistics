180102ca66c001dd277ab35a4990bd04d0eac0fa
Simon Andreassen <simo531i@elevcampus.dk>
2021-01-20 09:44:03 +0100

    Add project files.

---
+++ WeatherStatistics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeatherStatistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
