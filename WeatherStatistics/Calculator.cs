d9fe19da08b8947b35e4c34ee6ac49d58299b3bc
Simon Andreassen <simo531i@elevcampus.dk>
2021-01-20 11:16:23 +0100

    v1.0.0 Add initial code.

---
+++ WeatherStatistics/Calculator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherStatistics
+{
+    class Calculator
+    {
+        /// <summary>
+        /// Get the average value from a collection of double.
+        /// </summary>
+        public static double Average(List<double> numbers)
+        {
+            return Average(numbers.ToArray());
+        }
+
+        /// <summary>
+        /// Get the average value from a collection of double.
+        /// </summary>
+        public static double Average(double[] numbers)
+        {
+            double sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+
+            return sum / numbers.Length;
+        }
+
+        /// <summary>
+        /// Get the highest number in a collection.
+        /// </summary>
+        public static double High(List<double> numbers)
+        {
+            return High(numbers.ToArray());
+        }
+
+        /// <summary>
+        /// Get the highest number in a collection.
+        /// </summary>
+        public static double High(double[] numbers)
+        {
+            double high = Double.MinValue;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > high)
+                    high = numbers[i];
+            }
+
+            return high;
+        }
+
+        /// <summary>
+        /// Get the lowest number in a collection.
+        /// </summary>
+        public static double Low(List<double> numbers)
+        {
+            return Low(numbers.ToArray());
+        }
+
+        /// <summary>
+        /// Get the lowest number in a collection.
+        /// </summary>
+        public static double Low(double[] numbers)
+        {
+            double low = Double.MaxValue;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] < low)
+                    low = numbers[i];
+            }
+
+            return low;
+        }
+    }
+}
--- WeatherStatistics/Program.cs
+++ WeatherStatistics/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading;
 
 namespace WeatherStatistics
 {
     class Program
     {
+        private static List<double> measurements = new List<double>();
+        
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {          
+            // If no provited args from application launch, ask for them now.
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Enter measurements:");
+                args = new Regex("[ ]{2,}", RegexOptions.None).Replace(Console.ReadLine(), " ").Split(' ');
+            }
+
+            // Try to convert each argument to double and add it to measurements list.
+            foreach (var arg in args)
+            {
+                if (Double.TryParse(arg.Replace('.', ','), out double measurement))
+                    measurements.Add(measurement);
+            }
+
+            // Print added measurements.
+            Console.WriteLine("\nADDED MEASUREMENTS:");
+            foreach (var measurement in measurements)
+            {
+                Console.WriteLine(measurement);
+            }
+
+            // Print summary.
+            Console.WriteLine("\nSUMMARY:\n" +
+                "Average: " + Calculator.Average(measurements) + "\n" +
+                "High: " + Calculator.High(measurements) + "\n" +
+                "Low: " + Calculator.Low(measurements));
         }
     }
 }
